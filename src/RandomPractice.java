74afcba3b91d98828542ec6750e7023bb9e32e6a
Yuan Xue <ziyaajiang@gmail.com>
2014-03-23 19:49:42 +0800

    more

--- src/RandomPractice.java
+++ src/RandomPractice.java
@@ -4,8 +4,46 @@ public class RandomPractice {
 		return a + StdRandom.random() * (b - a);
 	}
 	
-	public static void main(String[] args){
-		System.out.println(uniform(3.0,4.0));
+	@SuppressWarnings("deprecation")
+	public static int uniform(int N){
+		return (int) (StdRandom.random() * N);
 	}
-
+	
+	public static int uniform(int lo, int hi){
+		return lo + StdRandom.uniform(hi - lo);
+	}
+	
+	public static int discrete(double[] a){
+		//Entries in a[] must sum to 1;
+		@SuppressWarnings("deprecation")
+		double r = StdRandom.random();
+		double sum = 0.0;
+		for (int i = 0; i < a.length; i++){
+			sum = sum + a[i];
+			if (sum >= r) return i;
+		}
+		return -1;
+	}
+	
+	public static double[] shuffle(double[] a){
+		int N = a.length;
+		for (int i = 0; i < N; i++){
+			int r = i + StdRandom.uniform(N-i);
+			double temp = a[i];
+			a[i] = a[r];
+			a[r] = temp;
+		}
+		return a;
+	}
+	
+	public static void main(String[] args){
+		System.out.println(uniform(157,298));
+		double[] a = {0.1, 0.2, 0.3, 0.2, 0.2};
+		System.out.println(discrete(a));
+		double[] b = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+		double[] c = shuffle(b);
+		for (int i = 0; i < c.length; i++ ){
+			System.out.println(c[i]);
+		}
+	}
 }
