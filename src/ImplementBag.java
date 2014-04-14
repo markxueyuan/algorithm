40c91bd4cf719def2d489f07c84840b7825225b0
Yuan Xue <ziyaajiang@gmail.com>
2014-04-14 22:55:58 +0800

    ADT Bag

---
+++ src/ImplementBag.java
@@ -0,0 +1,11 @@
+
+public class ImplementBag<Item> implements Iterable<Item> {
+	
+	public ImplementBag(){};
+	
+	public void add(Item item){};
+	
+	public boolean isEmpty(){};
+	
+	public int size(){};
+}
